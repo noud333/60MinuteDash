443c0d2299ec6cffde53a61ad4fde084fd2a9e1f
Noud Hover <noud.hover@gmail.com>
2022-01-14 09:18:59 +0100

    code opgeschoond

--- beta_noud/main.py
+++ beta_noud/main.py
@@ -4,24 +4,48 @@ This function runs the rush hour game via the board class
 import time
 from board import Board
 
-
-begin = time.time()
-test = Board(6, "Rushhour6x6_1.csv")
-test.print_board()
-
-while True:
-
-    # move = input("Next move: ").split()
-
-    # print(test.move_car(move[0], int(move[1])))
-    # test.print_board()
-    test.random_move()
-
-    if test.victory():
-        print("Congrats")
-        test.save_output()
+class Game():
+    def __init__(self, size, name):
+        self.size = size
+        self.name = name
+    
+    def random_moves(self):
+        begin = time.time()
+        test = Board(self.size, self.name)
         test.print_board()
-        test.generate_frame()
-        end = time.time()
-        print(end - begin)
-        break
+
+        while True:
+            test.random_move()
+
+            if test.victory():
+                print("Congrats")
+                test.save_output()
+                test.print_board()
+                test.generate_frame()
+                end = time.time()
+                print(end - begin)
+                break
+
+    def user_played(self):
+        begin = time.time()
+        test = Board(self.size, self.name)
+        test.print_board()
+
+        while True:
+
+            move = input("Next move: ").split()
+
+            print(test.move_car(move[0], int(move[1])))
+            test.print_board()
+
+            if test.victory():
+                print("Congrats")
+                test.save_output()
+                test.print_board()
+                test.generate_frame()
+                end = time.time()
+                print(end - begin)
+                break
+
+game = Game(6, "Rushhour6x6_1.csv")
+game.random_moves()
